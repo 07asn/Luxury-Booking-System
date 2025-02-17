ab2edb2e07b746c61ec00ab25e5df0f5c3f3cd2b
07ASN <HASAN.OMAR.20002@GMAIL.COM>
2025-02-17 20:56:22 +0300

    Deploy

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store} basename={import.meta.env.BASE_URL}>
       <App />
     </Provider>
   </React.StrictMode>
