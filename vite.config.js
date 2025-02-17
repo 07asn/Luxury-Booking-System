b07a78ed7c06071b40f15141010de83d63f9e2e8
07ASN <HASAN.OMAR.20002@GMAIL.COM>
2025-02-17 20:51:09 +0300

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/Luxury-Booking-System/",
+  build: {
+    sourcemap: false,
+  },
 });
